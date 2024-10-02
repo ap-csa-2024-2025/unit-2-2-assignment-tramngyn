8066d88dda9e20ba5f213935da09ce6dabc33306
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-10-02 10:58:56 -0700

    finished two

--- Classwork.java
+++ Classwork.java
@@ -2,6 +2,12 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    // Quote
+    System.out.println("\"That brain of mine is something more than merely mortal; as time will show.\"\n Ada Lovelace\n The first computer programmer");
+
+    System.out.println();
+   
+    // Rabit
+    System.out.println("(\\(\\\n( - -)\n((') (')");
   }
 }
